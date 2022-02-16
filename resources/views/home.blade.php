aa2867b35e01c6574db0c856c471c21fe52538af
Yaniss Guendouzi <guendouziyaniss@gmail>
2022-02-16 20:45:46 +0100

    working on home screen

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Tourismo</title>
+
+        <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js" defer></script>
+    </head>
+    <body >
+        <nav>
+            <h1>Tourismo</h1>
+            <ul>
+                <li><a href="/">Accueil</a></li>
+                <li><a href="/">Activités</a></li>
+                <li><a href="/">Places</a></li>
+            </ul>
+        </nav>
+        <div class="bg-image">
+            <h5 class="intro-text">Explorez de beaux endroits et profitez de merveilleuses activités</h5>
+            <a href="#main" class="explore-btn">Explorez</a>
+            <div class="white-ora"></div>
+        </div>
+        <div id="main" >
+            <h6>Meilleurs endroits</h6>        
+    </div>
+    </body>
+</html>
