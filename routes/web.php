8df682b9818e2c43d5fa81aa3637a432e06e6c5e
Yaniss Guendouzi <guendouziyaniss@gmail>
2022-02-17 22:06:01 +0100

    added auth screens

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/register', function () {
+    return view('register');
+});
