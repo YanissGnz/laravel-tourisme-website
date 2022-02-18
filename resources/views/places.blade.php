3c37b2dc53e8bc5907d66fa886600f415984ca3b
Yaniss Guendouzi <guendouziyaniss@gmail>
2022-02-18 12:06:36 +0100

    added all places page

---
+++ resources/views/places.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Tourismo</title>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="stylesheet" href="/css/places.css">
+    </head>
+<body >
+    <nav>
+        <h1 class="logo"><a href="/">Tourismo</a></h1>
+            <ul>
+                <li><a href="/">Accueil</a></li>
+                <li><a href="/places">Places</a></li>
+                <li><a href="/activities">Activités</a></li>
+                <li ><a href="/login" >Login</a></li>
+            </ul>
+        </nav>
+        <div id="main" >
+            <h6 class="main-title">Tous les endroits</h6>    
+            <div class="places-card-list">
+                <div class="card">
+                    <img src="/places/place1.jpg" alt="Place image" class="card-img">
+                    <div class="card-content">
+                        
+                        <h1 class="card-title">
+                            Explorez 
+                        </h1>
+                        <p class="card-body">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, iusto  Lorem ipsum dolor sit amet.
+                        </p>
+                        <a href="#" class="card-btn">Explorez</a>
+                     </div>
+                </div>  <div class="card">
+                    <img src="/places/place1.jpg" alt="Place image" class="card-img">
+                    <div class="card-content">
+                        
+                        <h1 class="card-title">
+                            Explorez 
+                        </h1>
+                        <p class="card-body">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, iusto  Lorem ipsum dolor sit amet.
+                        </p>
+                        <a href="#" class="card-btn">Explorez</a>
+                     </div>
+                </div>  <div class="card">
+                    <img src="/places/place1.jpg" alt="Place image" class="card-img">
+                    <div class="card-content">
+                        
+                        <h1 class="card-title">
+                            Explorez 
+                        </h1>
+                        <p class="card-body">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, iusto  Lorem ipsum dolor sit amet.
+                        </p>
+                        <a href="#" class="card-btn">Explorez</a>
+                     </div>
+                </div>  <div class="card">
+                    <img src="/places/place1.jpg" alt="Place image" class="card-img">
+                    <div class="card-content">
+                        
+                        <h1 class="card-title">
+                            Explorez 
+                        </h1>
+                        <p class="card-body">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, iusto  Lorem ipsum dolor sit amet.
+                        </p>
+                        <a href="#" class="card-btn">Explorez</a>
+                     </div>
+                </div> 
+            </div>
+            
+            <footer>
+                Tourismo &copy; 2022
+        </footer>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js" ></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js"></script>
+        <script src="/js/app.js" defer></script>
+        <script>
+           
+        </script>
+        
+    </body>
+</html>
+    
